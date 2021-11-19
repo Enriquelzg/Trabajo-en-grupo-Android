61da6523ffe3c49f02c39803f6eeca0e917a362a
Zenit2003 <francobusquets@gmail.com>
2021-11-19 15:10:50 +0100

    Prueba Franco

--- AppNotas/app/src/main/java/com/example/appnotas/MainActivity.java
+++ AppNotas/app/src/main/java/com/example/appnotas/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.appnotas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button boton1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton1 =findViewById(R.id.boton_prueba);
+
     }
 }
