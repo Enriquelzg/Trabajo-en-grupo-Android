d38b9a7a086ba27af7429dbba2f1faf33e545e7b
Enriquelzg <enrique.lazaro@alumnos.upm.es>
2021-11-19 15:21:08 +0100

    Prueba

--- AppNotas/app/src/main/java/com/example/appnotas/MainActivity.java
+++ AppNotas/app/src/main/java/com/example/appnotas/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.appnotas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView prueba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        prueba = findViewById(R.id.textoprueba);
     }
 }
