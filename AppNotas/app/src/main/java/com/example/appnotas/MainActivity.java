33a53ecded0709c3d1cad8fc8e4a3839797c3d38
Zenit2003 <francobusquets@gmail.com>
2021-11-23 15:05:44 +0100

    reinicio main

--- AppNotas/app/src/main/java/com/example/appnotas/MainActivity.java
+++ AppNotas/app/src/main/java/com/example/appnotas/MainActivity.java
@@ -3,11 +3,10 @@ package com.example.appnotas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button boton1;
+
 
 
 
@@ -16,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        boton1 =findViewById(R.id.boton_prueba);
 
     }
 }
