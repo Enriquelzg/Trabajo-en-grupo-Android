66a8d07836d9ca3983264df22a49ebf8b108d71d
Enriquelzg <enrique.lazaro@alumnos.upm.es>
2021-11-23 16:00:00 +0100

    cambioAct

--- AppNotas/app/src/main/java/com/example/appnotas/MainActivity.java
+++ AppNotas/app/src/main/java/com/example/appnotas/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.appnotas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button nuevaNota;
+    private List<String> listaNotas = new ArrayList<String>();
+    private String nota;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nuevaNota = findViewById(R.id.nueva);
+
+        nuevaNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Notas.class));
+            }
+        });
     }
 }
