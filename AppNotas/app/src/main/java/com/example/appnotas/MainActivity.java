d94b2c9955a9c19eb9eadbc4c6970cb779cb3c55
Enriquelzg <enrique.lazaro@alumnos.upm.es>
2021-11-23 20:05:48 +0100

    5 notas no editables

--- AppNotas/app/src/main/java/com/example/appnotas/MainActivity.java
+++ AppNotas/app/src/main/java/com/example/appnotas/MainActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.material.transformation.ExpandableBehavior;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton nuevaNota;
     private List<String> listaNotas = null;
     public String nota;
-    private EditText texto;
+    private TextView texto,texto1,texto2,texto3,texto4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +31,38 @@ public class MainActivity extends AppCompatActivity {
         nuevaNota = findViewById(R.id.nueva);
         texto = findViewById(R.id.nota);
         listaNotas = Notas.getListaNotas();
+        texto1 = findViewById(R.id.texto1);
+        texto2 = findViewById(R.id.texto2);
+        texto3 = findViewById(R.id.texto3);
+        texto4 = findViewById(R.id.texto4);
 
         try {
             nota=listaNotas.get(0);
             texto.setText(nota);
+            try {
+                nota=listaNotas.get(1);
+                texto1.setText(nota);
+                try {
+                    nota=listaNotas.get(2);
+                    texto2.setText(nota);
+                    try {
+                        nota=listaNotas.get(3);
+                        texto3.setText(nota);
+                        try {
+                            nota=listaNotas.get(4);
+                            texto4.setText(nota);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
         catch (Exception e){
             e.printStackTrace();
