f8cb6822855f7e3a919031922c708045c20f9403
Enriquelzg <enrique.lazaro@alumnos.upm.es>
2021-11-23 14:56:47 +0100

    reinicio

--- AppNotas/app/src/main/java/com/example/appnotas/MainActivity.java
+++ AppNotas/app/src/main/java/com/example/appnotas/MainActivity.java
@@ -4,25 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import android.widget.TextView;
-
-import android.widget.Button;
-
 
 public class MainActivity extends AppCompatActivity {
-    private TextView prueba;
-
-    private Button boton1;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        prueba = findViewById(R.id.textoprueba);
-        boton1 =findViewById(R.id.boton_prueba);
-
-
     }
 }
