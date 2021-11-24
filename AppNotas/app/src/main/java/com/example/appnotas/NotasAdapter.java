90d347dccf68f061c7b155fdad83bd70298c4e46
Enriquelzg <enrique.lazaro@alumnos.upm.es>
2021-11-24 15:52:07 +0100

    Más avances del recyclerView

--- AppNotas/app/src/main/java/com/example/appnotas/NotasAdapter.java
+++ AppNotas/app/src/main/java/com/example/appnotas/NotasAdapter.java
@@ -1,7 +1,9 @@
 package com.example.appnotas;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,22 +14,27 @@ public class NotasAdapter extends RecyclerView.Adapter<NotasAdapter.ViewHolder> 
     @NonNull
     @Override
     public NotasAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.formanotas, parent, false);
+
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull NotasAdapter.ViewHolder holder, int position) {
-
+        holder.tituloText.setText(ConstructorNotas.getTitulo());
     }
 
     @Override
     public int getItemCount() {
-        return ;
+        return 0;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tituloText;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tituloText = itemView.findViewById(R.id.textView);
         }
     }
     ArrayList<Object>notas;
